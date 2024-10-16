4d9ac0675544a6e4e1f01096fd9f154cf425336f
Danny Fritz <me@dannyfritz.com>
2024-10-16 14:45:15 -0500

    PostHeader layout

---
+++ .vitepress/theme/PostHeader.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useData } from "vitepress";
+import { format } from "date-fns";
+const postDate = format(
+  new Date(useData().frontmatter.value.date),
+  "yyyy-MM-dd",
+);
+</script>
+
+<template>
+  <h1>
+    {{ $frontmatter.title }}
+  </h1>
+  <p v-if="postDate">
+    Published on <time>{{ postDate }}</time>
+  </p>
+  <blockquote>
+    <p v-if="$frontmatter.summary">{{ $frontmatter.summary }}</p>
+  </blockquote>
+</template>
